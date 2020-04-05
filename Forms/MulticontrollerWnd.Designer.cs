0f271433dcbe4c2e473d18fb30f728eb86a28190
Daniel <dan@danfresneda.com>
2020-04-05 12:21:53 -0400

    Tweaked main UI controls

--- Forms/MulticontrollerWnd.Designer.cs
+++ Forms/MulticontrollerWnd.Designer.cs
@@ -39,10 +39,10 @@
             this.leftStatusLbl = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
             this.rightStatusLbl = new System.Windows.Forms.ToolStripStatusLabel();
-            this.rightToonCrosshair = new TTMulti.Controls.SelectWindowCrosshair();
-            this.leftToonCrosshair = new TTMulti.Controls.SelectWindowCrosshair();
             this.panel1 = new System.Windows.Forms.Panel();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.rightToonCrosshair = new TTMulti.Controls.SelectWindowCrosshair();
+            this.leftToonCrosshair = new TTMulti.Controls.SelectWindowCrosshair();
             this.statusStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -54,9 +54,10 @@
             this.mirrorModeRadio.FlatAppearance.BorderSize = 3;
             this.mirrorModeRadio.FlatAppearance.CheckedBackColor = System.Drawing.Color.Violet;
             this.mirrorModeRadio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.mirrorModeRadio.Location = new System.Drawing.Point(109, 0);
+            this.mirrorModeRadio.Location = new System.Drawing.Point(136, 0);
+            this.mirrorModeRadio.Margin = new System.Windows.Forms.Padding(4);
             this.mirrorModeRadio.Name = "mirrorModeRadio";
-            this.mirrorModeRadio.Size = new System.Drawing.Size(104, 27);
+            this.mirrorModeRadio.Size = new System.Drawing.Size(130, 34);
             this.mirrorModeRadio.TabIndex = 3;
             this.mirrorModeRadio.Text = "Mirror Mode";
             this.mirrorModeRadio.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -73,8 +74,9 @@
             this.multiModeRadio.FlatAppearance.CheckedBackColor = System.Drawing.Color.LimeGreen;
             this.multiModeRadio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.multiModeRadio.Location = new System.Drawing.Point(0, 0);
+            this.multiModeRadio.Margin = new System.Windows.Forms.Padding(4);
             this.multiModeRadio.Name = "multiModeRadio";
-            this.multiModeRadio.Size = new System.Drawing.Size(104, 27);
+            this.multiModeRadio.Size = new System.Drawing.Size(130, 34);
             this.multiModeRadio.TabIndex = 6;
             this.multiModeRadio.TabStop = true;
             this.multiModeRadio.Text = "Multi-Mode";
@@ -97,9 +99,10 @@
             this.optionsBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.optionsBtn.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.optionsBtn.Location = new System.Drawing.Point(54, 30);
+            this.optionsBtn.Location = new System.Drawing.Point(68, 38);
+            this.optionsBtn.Margin = new System.Windows.Forms.Padding(4);
             this.optionsBtn.Name = "optionsBtn";
-            this.optionsBtn.Size = new System.Drawing.Size(122, 20);
+            this.optionsBtn.Size = new System.Drawing.Size(152, 25);
             this.optionsBtn.TabIndex = 8;
             this.optionsBtn.Text = "Options";
             this.optionsBtn.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -111,9 +114,10 @@
             this.windowGroupsBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.windowGroupsBtn.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.windowGroupsBtn.Location = new System.Drawing.Point(54, 6);
+            this.windowGroupsBtn.Location = new System.Drawing.Point(68, 8);
+            this.windowGroupsBtn.Margin = new System.Windows.Forms.Padding(4);
             this.windowGroupsBtn.Name = "windowGroupsBtn";
-            this.windowGroupsBtn.Size = new System.Drawing.Size(122, 20);
+            this.windowGroupsBtn.Size = new System.Drawing.Size(152, 25);
             this.windowGroupsBtn.TabIndex = 12;
             this.windowGroupsBtn.Text = "Window Groups";
             this.windowGroupsBtn.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -131,11 +135,11 @@
             this.leftStatusLbl,
             this.toolStripStatusLabel2,
             this.rightStatusLbl});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 90);
-            this.statusStrip1.Margin = new System.Windows.Forms.Padding(0, 25, 0, 0);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 112);
+            this.statusStrip1.Margin = new System.Windows.Forms.Padding(0, 31, 0, 0);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 15, 0);
-            this.statusStrip1.Size = new System.Drawing.Size(230, 22);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(288, 28);
             this.statusStrip1.SizingGrip = false;
             this.statusStrip1.TabIndex = 15;
             this.statusStrip1.Text = "statusStrip1";
@@ -143,59 +147,62 @@
             // leftStatusLbl
             // 
             this.leftStatusLbl.Name = "leftStatusLbl";
-            this.leftStatusLbl.Size = new System.Drawing.Size(0, 17);
+            this.leftStatusLbl.Size = new System.Drawing.Size(0, 22);
             // 
             // toolStripStatusLabel2
             // 
             this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
-            this.toolStripStatusLabel2.Size = new System.Drawing.Size(214, 17);
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(268, 22);
             this.toolStripStatusLabel2.Spring = true;
             this.toolStripStatusLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // rightStatusLbl
             // 
             this.rightStatusLbl.Name = "rightStatusLbl";
-            this.rightStatusLbl.Size = new System.Drawing.Size(0, 17);
+            this.rightStatusLbl.Size = new System.Drawing.Size(0, 22);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.mirrorModeRadio);
+            this.panel1.Controls.Add(this.multiModeRadio);
+            this.panel1.Location = new System.Drawing.Point(11, 70);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(266, 34);
+            this.panel1.TabIndex = 7;
             // 
             // rightToonCrosshair
             // 
             this.rightToonCrosshair.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.rightToonCrosshair.BackColor = System.Drawing.SystemColors.Control;
-            this.rightToonCrosshair.Location = new System.Drawing.Point(186, 10);
+            this.rightToonCrosshair.Location = new System.Drawing.Point(232, 12);
+            this.rightToonCrosshair.Margin = new System.Windows.Forms.Padding(4);
             this.rightToonCrosshair.Name = "rightToonCrosshair";
             this.rightToonCrosshair.Padding = new System.Windows.Forms.Padding(2);
             this.rightToonCrosshair.SelectedBorderColor = System.Drawing.Color.DarkGreen;
-            this.rightToonCrosshair.Size = new System.Drawing.Size(36, 36);
+            this.rightToonCrosshair.Size = new System.Drawing.Size(45, 45);
             this.rightToonCrosshair.TabIndex = 14;
             this.rightToonCrosshair.WindowSelected += new TTMulti.Controls.WindowSelectedHandler(this.rightToonCrosshair_WindowSelected);
             // 
             // leftToonCrosshair
             // 
             this.leftToonCrosshair.BackColor = System.Drawing.SystemColors.Control;
-            this.leftToonCrosshair.Location = new System.Drawing.Point(9, 10);
+            this.leftToonCrosshair.Location = new System.Drawing.Point(11, 12);
+            this.leftToonCrosshair.Margin = new System.Windows.Forms.Padding(4);
             this.leftToonCrosshair.Name = "leftToonCrosshair";
             this.leftToonCrosshair.Padding = new System.Windows.Forms.Padding(2);
             this.leftToonCrosshair.SelectedBorderColor = System.Drawing.Color.LimeGreen;
-            this.leftToonCrosshair.Size = new System.Drawing.Size(36, 36);
+            this.leftToonCrosshair.Size = new System.Drawing.Size(45, 45);
             this.leftToonCrosshair.TabIndex = 13;
             this.leftToonCrosshair.WindowSelected += new TTMulti.Controls.WindowSelectedHandler(this.leftToonCrosshair_WindowSelected);
             // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.mirrorModeRadio);
-            this.panel1.Controls.Add(this.multiModeRadio);
-            this.panel1.Location = new System.Drawing.Point(9, 56);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(213, 27);
-            this.panel1.TabIndex = 7;
-            // 
             // MulticontrollerWnd
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(228, 112);
+            this.ClientSize = new System.Drawing.Size(285, 140);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.rightToonCrosshair);
@@ -204,10 +211,11 @@
             this.Controls.Add(this.optionsBtn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MulticontrollerWnd";
-            this.Padding = new System.Windows.Forms.Padding(6, 6, 3, 26);
+            this.Padding = new System.Windows.Forms.Padding(8, 8, 4, 32);
             this.Text = "Toontown Multicontroller";
             this.Activated += new System.EventHandler(this.MulticontrollerWnd_Activated);
             this.Deactivate += new System.EventHandler(this.MulticontrollerWnd_Deactivate);
